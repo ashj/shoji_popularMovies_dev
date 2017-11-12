10179593a71c8473890ff5685ccdf3454f79fd62
lsitec333.asato <andre.shoji@lsitec.org.br>
2017-11-12 13:44:08 -0200

    set w/h for image view
    Signed-off-by: lsitec333.asato <andre.shoji@lsitec.org.br>

--- app/src/main/java/com/shoji/example/android/popularmoviesstage1/data/MoviesListViewHolder.java
+++ app/src/main/java/com/shoji/example/android/popularmoviesstage1/data/MoviesListViewHolder.java
@@ -20,8 +20,6 @@ public class MoviesListViewHolder
     private MovieDataViewHolderOnClickListener mMovieDataViewHolderOnClickListener;
 
     private ImageView mPoster_image;
-    //TODO(fix blank screen). this temporary fixes it. do better than this
-    private TextView mTitle_tv;
 
     public interface MovieDataViewHolderOnClickListener {
         void onClick(int position);
@@ -36,18 +34,12 @@ public class MoviesListViewHolder
         this.mContext = context;
         mPoster_image = itemView.findViewById(R.id.movie_poster_image_view);
 
-        //TODO(fix blank screen). this temporary fixes it. do better than this
-        mTitle_tv = itemView.findViewById(R.id.movie_poster_title_tv);
-
-
         itemView.setOnClickListener(this);
         mMovieDataViewHolderOnClickListener = movieDataViewHolderOnClickListener;
     }
 
     public void bindViewHolder(MovieData movieData) {
         mMovieData = movieData;
-        //TODO(fix blank screen). this temporary fixes it. do better than this
-        mTitle_tv.setText(movieData.getTitle());
 
         TheMovieDbUtils.loadImage(mContext,
                 mMovieData.getPosterPath(),
@@ -60,7 +52,6 @@ public class MoviesListViewHolder
     public void onClick(View view) {
         int adapterPosition = getAdapterPosition();
         //Log.d(TAG, "Tapped on position="+adapterPosition);
-
         mMovieDataViewHolderOnClickListener.onClick(adapterPosition);
     }
 }
