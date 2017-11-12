005d25f8b22c85a8c96ad7b24e0f770d3b6bd170
lsitec333.asato <andre.shoji@lsitec.org.br>
2017-11-12 20:29:21 -0200

    Content Provider step 02 - add URIs to the contract
    Signed-off-by: lsitec333.asato <andre.shoji@lsitec.org.br>

--- app/src/main/java/com/shoji/example/android/popularmoviesstage1/database/FavoriteMoviesContract.java
+++ app/src/main/java/com/shoji/example/android/popularmoviesstage1/database/FavoriteMoviesContract.java
@@ -1,14 +1,22 @@
 package com.shoji.example.android.popularmoviesstage1.database;
 
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class FavoriteMoviesContract {
-
+    public static final String AUTHORITY = "com.shoji.example.android.popularmoviesstage1";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    public static final String PATH_FAVORITE_MOVIES = FavoriteMoviesEntry.TABLE_NAME;
 
     private FavoriteMoviesContract() {}
 
     public static final class FavoriteMoviesEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_FAVORITE_MOVIES)
+                .build();
+
         public static final String TABLE_NAME = "favoritemovies";
 
         public static final String COLUMN_MOVIE_ID = "movie_id";
