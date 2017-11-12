cf4324a2f7c014881b551a4f03d26dca7f9b6643
lsitec333.asato <andre.shoji@lsitec.org.br>
2017-11-12 20:35:38 -0200

    Content Provider step 03 - create the URIMatcher
    Signed-off-by: lsitec333.asato <andre.shoji@lsitec.org.br>

--- app/src/main/java/com/shoji/example/android/popularmoviesstage1/database/FavoriteMoviesContentProvider.java
+++ app/src/main/java/com/shoji/example/android/popularmoviesstage1/database/FavoriteMoviesContentProvider.java
@@ -3,15 +3,20 @@ package com.shoji.example.android.popularmoviesstage1.database;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-
+import com.shoji.example.android.popularmoviesstage1.database.FavoriteMoviesContract.FavoriteMoviesEntry;
 
 
 public class FavoriteMoviesContentProvider extends ContentProvider {
     private FavoriteMoviesDbHelper mFavoriteMoviesDbHelper;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private static final int FAVORITES = 0;
+    private static final int FAVORITES_WITH_ID = 1;
 
     @Override
     public boolean onCreate() {
@@ -19,6 +24,25 @@ public class FavoriteMoviesContentProvider extends ContentProvider {
         mFavoriteMoviesDbHelper = new FavoriteMoviesDbHelper(context);
         return true;
     }
+
+
+
+
+    private static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(FavoriteMoviesContract.AUTHORITY,
+                FavoriteMoviesContract.PATH_FAVORITE_MOVIES,
+                FAVORITES);
+
+        uriMatcher.addURI(FavoriteMoviesContract.AUTHORITY,
+                FavoriteMoviesContract.PATH_FAVORITE_MOVIES + "/#",
+                FAVORITES_WITH_ID);
+
+        return uriMatcher;
+    }
+
+    
 
     @Nullable
     @Override
